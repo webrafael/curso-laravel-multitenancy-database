79ee97a68d32610a916f1fcb1bf98641fdbaa17e
Rafael Andrade <rafael.io@outlook.com>
2022-10-12 19:33:01 -0300

    Implementa rotas iniciais

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('404', function () {
+    return view('404-page');
+})->name('404');
+
 Route::get('/', function () {
     return view('welcome');
 });

b2b1229daff7651be2b737e2a91e12e8cae956fd
Rafael Andrade <rafael.io@outlook.com>
2022-10-12 19:31:06 -0300

    Implementa Tenant alternador de database

--- app/Http/Middleware/Tenant/TenantMiddleware.php
+++ app/Http/Middleware/Tenant/TenantMiddleware.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware\Tenant;
 
 use Closure;
+use App\Models\Company;
+use App\Tenant\ManagerTenant;
 use Illuminate\Http\Request;
 
 class TenantMiddleware
@@ -16,6 +18,21 @@ class TenantMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        $manager = app(ManagerTenant::class);
+
+        $company = $this->getCompany($request->getHost());
+
+        if(! $company && $request->url() != route('404')) {
+            return redirect()->route('404');
+        } else if ($request->url() != route('404') && !$manager->domainIsMain() ) {
+            $manager->setConnection($company);
+        }
+
         return $next($request);
+    }
+
+    public function getCompany(string $host): ?Company
+    {
+        return Company::where('domain', $host)->first();
     }
 }

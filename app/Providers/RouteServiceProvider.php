1bb365bcb43e45388c27853ba3fde5e1a9abf317
Rafael Andrade <rafael.io@outlook.com>
2022-10-12 19:33:24 -0300

    Implementa RouteServiceProviders para Tenants

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+            
+            Route::middleware('web', 'check.domain.main')
+                ->prefix('tenants')
+                ->group(base_path('routes/tenant.php'));
         });
     }
 

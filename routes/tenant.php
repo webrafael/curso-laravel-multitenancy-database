ef1e08aebad6961d82894c46d3e01c9317104edd
Rafael Andrade <rafael.io@outlook.com>
2022-10-12 21:27:59 -0300

    melhoria nas rotas

--- routes/tenant.php
+++ routes/tenant.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('company/store', [\App\Http\Controllers\Tenant\CompanyController::class, 'store'])->name('company.store');
+
 Route::get('/tenant', function () {
     return "funcionou";
 })->name('tenant');
